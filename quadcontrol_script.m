b63d18d802174076fbbf03ee87cb0329c3708870
Lucas <lucasvgoncalves@tecnico.ulisboa.pt>
2022-12-14 16:28:49 +0000

    Updates

--- quadcontrol_script.m
+++ quadcontrol_script.m
@@ -28,25 +28,25 @@
 
 %% 1.5
 
-natural_freq = 2*pi/5; %rad/s
-csi= [0.1 0.2 0.5 0.9 1 2 4]; 
-damping_coefficient =0;
-
-for i= 1:7
-    damping_coefficient= csi (i); 
-    u_gain= natural_freq^2; 
-    y_gain =  natural_freq^2; 
-    y_dot_gain= 2* damping_coefficient * natural_freq;
-    simOut= sim ('quadrotor_control');
-    
-    hold on
-    plot(simOut.y.time, simOut.y.signals.values);
-    xlabel("t (s)");
-    ylabel("y");
-    grid on;
-    box on;
-    title("Example step response of a second-order LTI system")
-      
-end
+% natural_freq = 2*pi/5; %rad/s
+% csi= [0.1 0.2 0.5 0.9 1 2 4]; 
+% damping_coefficient =0;
+% 
+% for i= 1:7
+%     damping_coefficient= csi (i); 
+%     u_gain= natural_freq^2; 
+%     y_gain =  natural_freq^2; 
+%     y_dot_gain= 2* damping_coefficient * natural_freq;
+%     simOut= sim ('quadrotor_control');
+%     
+%     hold on
+%     plot(simOut.y.time, simOut.y.signals.values);
+%     xlabel("t (s)");
+%     ylabel("y");
+%     grid on;
+%     box on;
+%     title("Example step response of a second-order LTI system")
+%       
+% end
 
 
---
+++ script.m
@@ -0,0 +1,10 @@
+%% Laboratory 2
+
+%Initialization of variables
+M = 1; %kg
+G = 9.8; %m/s^2
+Kt = 3.575*10^(-5); %N/(rad/s)^2
+
+Z_0 = 2; 
+Omega_0 =  sqrt((G*M)/Kt);
+U_0 = Omega_0;
