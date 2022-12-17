5531e7c7e548fce0e5dc96ea352f6d4f24b7bdd5
Lucas <lucasvgoncalves@tecnico.ulisboa.pt>
2022-12-17 00:26:54 +0000

    Shit

--- LAB3/script_lab3.m
+++ LAB3/script_lab3.m
@@ -1,4 +1,4 @@
-%% Laboratory 3
+%% Laboratory 3 - Session 5
 
 close all;
 clear;
@@ -14,7 +14,10 @@ Kt = 3.575*10^(-5); %N/(rad/s)^2
 Z_0 = 2; 
 Omega_0 =  sqrt((G*M)/Kt);
 U_0 = Omega_0;
-dz_r=0;
 
-s= tf(-1, [1 0 0]); %transfer function 1/(s^2)
-rlocus (s)
+%% 3.4 Root-Locus of a transfer function
+
+%transfer function 1/(s^2)
+
+ s= tf(-1, [1 0 0]); 
+ rlocus (s)
